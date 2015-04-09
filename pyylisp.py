d3b00d77f3d9d0e98e0f805ba2d351d76ce53765
Mark Haferkamp <github.com@refola.com>
2015-04-09 00:53:07 -0700

    refactoring incomplete code, adding Umbrello class diagram

--- pyylisp.py
+++ pyylisp.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 '''
 pyylisp.py
 Author: Mark Haferkamp
-Last date modified: 2015-04-06
+Last date modified: 2015-04-08
 Interpret a Pythony dialect of Lisp.
 
 Pyylisp is a scripting language that uses a parentheses-light Lisp syntax and macro system to run Python functions.
@@ -10,67 +11,43 @@ It's essentially Pythonic syntax and features combined with Lisp semantics and l
 '''
 
 import sys
+import tokens
+import parser
+import compiler
 
-class Token(object):
-	"""Representation of a single token in a parse of a Pyylisp file."""
+def getLines(filename):
+	'''Get the lines of text in a file, without line breaks.'''
+	f = open(filename, 'rt')
+	lines = list(l in f)
+	return lines
 
-	# Types of token
-	GROUPING = "grouping"
-	LIST_OPERATION = "list operation"
-	QUOTE = "quote"
-	COMMENT = "comment"
-	SYMBOL = "symbol"
-
-	def __init__(self, _type, content):
-		self._type = _type
-		self.content = content
-
-# Grouping tokens, the thing that Lisp parentheses and Python indentation means. Content is given as parentheses to be lazy; outputted .py code doesn't have to look pretty.
-START_GROUP = Token(Token.GROUPING, '(')
-END_GROUP = Token(Token.GROUPING, ')')
-
-def indentLevel(line):
-	"""Returns the indentation level of a line, defined in Pyylisp as the number of leading tabs."""
-	for i in range(len(line)):
-	        if line[i] != "\t":
-			return i # i characters were "\t" before lines[i]
-	return None # only whitespace -> nothing is indented
-
-def tokenize(lines):
-	"""Iterates over an iterable of lines (e.g., a file object in text mode) and returns a list of tokens."""
-	indentLevel = 0 # The level of indentation we're at, necessary for inferring parentheses from indentation
-	tokens = []
-	prevLineStart = 0 # Index of previous line's start, for inserting indentation-based START_GROUP tokens when the indentation level increases.
-	def insertIndentStarts(n):
-	        for i in range(n):
-			tokens.insert(prevLineStart, START_GROUP)
-	for line in lines:
-		newPrevLineStart = len(tokens)
-		line = line[:-1] # strip trailing '\n'; we're after the lines, not the separators between them
-		if not hasContent(line):
-			continue # skip blank or comment-only lines
-		i = indentLevel(line)
-		if i == None:
-			continue 
-		deltaIndent = i - indentLevel
-		if deltaIndent > 0:
-			insertIndentStarts(deltaIndent)
-		prevLineStart = newPrevLineStart
+def process(filename):
+	'''Process a single file. Takse the name of a .pyl file and saves the result to a .py file.'''
+	lineList = getLines(filename)
+	tokenList = tokens.convert(lineList)
+	parseTree = parser.parseTokens(tokenList)
+	compiled = compiler.compileTree(parseTree)
+	writeFile(compiled)
 
 def usage():
-	print("Usage: %s pyl-file-1 [pyl-file-2 [pyl-file-3 [...]]]" % sys.argv[0])
+	'''Show how to use the program.'''
+	print("Usage: %s pyl-file-1 [pyl-file-2 [...]]" % sys.argv[0])
+	print()
 	print("Converts given .pyl Pyylisp files into .py Python files.")
 	print("You probably want to run this as '%s *.pyl'." % sys.argv[0])
 
 def main():
+	'''Figure out what to do.'''
 	args=sys.argv[1:]
 	if len(args) == 0:
 		usage()
 		return
 	for arg in args:
 		if arg[-4:] != '.pyl':
+			print("Invalid Pyylisp file: %s." % arg)
 			usage()
 			return
-	map(convert, args)
+	for arg in args:
+		process(arg)
 
 main()
---
+++ tokens.py
@@ -0,0 +1,54 @@
+'''
+tokenize.py
+Author: Mark Haferkamp
+Last modified: 2015-04-08
+
+Convert a bunch of text into Pyylisp tokens.
+'''
+
+class Token(object):
+	"""Representation of a single token in a parse of a Pyylisp file."""
+
+	# Types of token
+	GROUPING = "grouping"
+	LIST_OPERATION = "list operation"
+	QUOTE = "quote"
+	COMMENT = "comment"
+	SYMBOL = "symbol"
+
+	def __init__(self, _type, content):
+		self._type = _type
+		self.content = content
+
+# Grouping tokens, the thing that Lisp parentheses and Python indentation means. Content is given as parentheses to be lazy; outputted .py code doesn't have to look pretty.
+START_GROUP = Token(Token.GROUPING, '(')
+END_GROUP = Token(Token.GROUPING, ')')
+
+def indentLevel(line):
+	"""Returns the indentation level of a line, defined in Pyylisp as the number of leading tabs."""
+	for i in range(len(line)):
+		if line[i] != "\t":
+			return i # i characters were "\t" before lines[i]
+	return None # only whitespace -> nothing is indented
+
+def convert(lines):
+	"""Iterates over an iterable of lines (e.g., a file object in text mode) and returns a list of tokens."""
+	indentLevel = 0 # The level of indentation we're at, necessary for inferring parentheses from indentation
+	tokens = []
+	prevLineStart = 0 # Index of previous line's start, for inserting indentation-based START_GROUP tokens when the indentation level increases.
+	def insertIndentStarts(n):
+		for i in range(n):
+			tokens.insert(prevLineStart, START_GROUP)
+	for line in lines:
+		newPrevLineStart = len(tokens)
+		line = line[:-1] # strip trailing '\n'; we're after the lines, not the separators between them
+		if not hasContent(line):
+			continue # skip blank or comment-only lines
+		i = indentLevel(line)
+		if i == None:
+			continue 
+		deltaIndent = i - indentLevel
+		if deltaIndent > 0:
+			insertIndentStarts(deltaIndent)
+		prevLineStart = newPrevLineStart
+
